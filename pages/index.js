d24b8c4a9c12fae2be2e5d78135f0b9a85b44fba
Nahiyan Khan <nahiyan.khan@gmail.com>
2022-05-06 10:27:39 -0400

    added prismic

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Head from 'next/head'
+import * as prismicH from "@prismicio/helpers";
+import { createClient } from "../prismicio";
+
 import Layout from '../components/layout'
 import Intro from '../components/intro'
 import Projects from '../components/projects'
 import About from '../components/about'
 import Contact from '../components/contact'
 
-export default function Home() {
+const Home = ({ page }) => {
   const siteTitle = 'Nahiyan Khan - Portfolio'
 
   return (
@@ -14,10 +17,26 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
 
+      <p>{prismicH.asText(page.data.tagline)}</p>
+
       <Intro />
       <About />
       <Projects />
       <Contact />
     </Layout>
   )
+}
+
+export default Home
+
+export async function getStaticProps({ locale, previewData }) {
+  const client = createClient({ previewData });
+
+  const page = await client.getByUID("homepage", "homepage", { lang: locale });
+
+  return {
+    props: {
+      page
+    },
+  };
 }
