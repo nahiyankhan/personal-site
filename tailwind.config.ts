a0644fd9cd0ea98db26e49ebabaf95c9a5e33e2d
Nahiyan Khan <nahiyan@Nahiyans-MacBook-Air.local>
2025-07-09 21:14:22 -0400

    animation tweaks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,11 +23,12 @@ export default {
 	plugins: [
 		require("@tailwindcss/typography"),
 		require("@tailwindcss/aspect-ratio"),
-		require('tailwind-clip-path'),
+		require("tailwind-clip-path"),
 		plugin(({ addComponents }) => {
 			addComponents({
 				".header-standard": {
-					"@apply text-lg sm:text-xl leading-6 sm:leading-9 font-light tracking-tight text-textDefault": {},
+					"@apply text-lg sm:text-xl leading-6 sm:leading-9 font-light tracking-tight text-textDefault":
+						{},
 				},
 				".subtitle-large": {
 					"@apply leading-[1.6] sm:leading-[1.75] text-textDefault tracking-wider": {},
@@ -37,11 +38,6 @@ export default {
 				},
 				".subsection-title": {
 					"@apply text-2xl font-extrabold text-textDefault sm:text-6xl uppercase": {},
-				},
-				".redact": {
-					"&::after": {
-						"@apply content-[''] absolute pointer-events-none left-0 w-[calc(100%+8px)] bg-[hsl(var(--redact-bg))] nk-transition h-[calc(100%+16px)] top-[-8px] mix-blend-exclusion origin-left scale-x-100 duration-500 bg-[repeating-linear-gradient(transparent,_transparent_3px,_#0000002b_4px)]": {},
-					},
 				},
 				".link": {
 					// "@apply redact bg-bgColor": {},
@@ -57,20 +53,8 @@ export default {
 						"@apply md:scale-x-100": {},
 					},
 				},
-				"button:not(.disabled)": {
-					// "@apply redact": {},
-					"&::after": {
-						"@apply translate-x-[100%] w-[calc(100%+8px)] h-[calc(100%+16px)] w-full h-full top-0 left-0  scale-x-0 duration-300": {},
-					},
-					"&:hover::after": {
-						"@apply md:scale-x-100 md:translate-x-0": {},
-					},
-				},
 				".nk-transition": {
 					"@apply [transition:all_.3s_cubic-bezier(0.55,0,1,0.45),color_0s_ease-in]": {},
-				},
-				".animated-border": {
-					"@apply [&::after]:absolute [&::before]:z-[-1] [&::after]:z-[-1] [&::before]:absolute [&::after]:bottom-0 [&::after]:left-0 [&::after]:h-[1px] [&::after]:w-full [&::after]:animate-[borderForward_.7s_cubic-bezier(0.55,0,1,0.45)_forwards] [&::after]:bg-divider [&::after]:content-[''] [&::before]:left-0 [&::before]:top-[-1px] [&::before]:h-[1px] [&::before]:w-full [&::before]:animate-[borderForward_.7s_cubic-bezier(0.55,0,1,0.45)_forwards] [&::before]:bg-divider [&::before]:content-['']": {},
 				},
 			});
 		}),
@@ -93,125 +77,26 @@ export default {
 				fadein: {
 					"0%": {
 						opacity: "0",
+						transform: "scale(0.99)",
 					},
 					"100%": {
 						opacity: "1",
+						transform: "scale(1)",
 					},
 				},
-				dashForward: {
+				translatein: {
 					"0%": {
-						strokeDashoffset: "1000",
-						stroke: "hsl(var(--border-divider) / 1)",
-						color: "transparent",
-						opacity: "1",
-					},
-					"50%": {
-						strokeDashoffset: "0",
-						stroke: "hsl(var(--border-divider) / 1)",
-						color: "transparent",
-						opacity: "1",
-					},
-					"80%": {
-						strokeDashoffset: "0",
-						stroke: "hsl(var(--border-divider) / 1)",
-						color: "transparent",
-						opacity: "1",
+						transform: "translateX(-100%)",
 					},
 					"100%": {
-						strokeDashoffset: "0",
-						stroke: "transparent",
-						color: "hsl(var(--text-prominent) / 1)",
-						opacity: "1",
+						transform: "translateX(0)",
 					},
 				},
-				dashBackward: {
-					"0%": {
-						strokeDashoffset: "0",
-						stroke: "transparent",
-						color: "hsl(var(--text-prominent) / 1)",
-						opacity: "1",
-					},
-					"20%": {
-						stroke: "hsl(var(--border-divider) / 1)",
-						color: "transparent",
-						opacity: "1",
-					},
-					"100%": {
-						strokeDashoffset: "1000",
-						stroke: "hsl(var(--border-divider) / 1)",
-						color: "transparent",
-						opacity: "1",
-					},
-				},
-				dash: {
-          "0%": {
-            strokeDashoffset: "1000",
-            stroke: "hsl(var(--border-divider) / 1)",
-            color: "transparent",
-            opacity: "1",
-          },
-          "70%": {
-            strokeDashoffset: "0",
-            stroke: "hsl(var(--border-divider) / 1)",
-            color: "transparent",
-            opacity: "1",
-          },
-          "85%": {
-            strokeDashoffset: "0",
-            stroke: "hsl(var(--border-divider) / 1)",
-            color: "transparent",
-            opacity: "1",
-          },
-          "90%": {
-            strokeDashoffset: "0",
-            stroke: "transparent",
-            color: "hsl(var(--text-standard) / 1)",
-            opacity: "1",
-          },
-          "92.5%": {
-            strokeDashoffset: "0",
-            stroke: "hsl(var(--border-divider) / 1)",
-            color: "transparent",
-            opacity: "1",
-          },
-          "95%": {
-            strokeDashoffset: "0",
-            stroke: "hsl(var(--border-divider) / 1)",
-            color: "transparent",
-            opacity: "1",
-          },
-          "100%": {
-            strokeDashoffset: "0",
-            stroke: "transparent",
-            color: "hsl(var(--text-prominent) / 1)",
-            opacity: "1",
-          },
-        },
-				flicker: {
-					"0%": {
-						color: "hsl(var(--text-prominent) / 1)",
-						stroke: "transparent",
-						strokeDashoffset: "0",
-					},
-					"100%": {
-						color: "hsl(var(--text-prominent) / 0)",
-						stroke: "hsl(var(--border-divider) / 1)",
-						strokeDashoffset: "0",
-					}
-				},
-				borderForward: {
-					"0%": {
-						width: "0px",
-					},
-					"100%": {
-						width: "100%",
-					},
-				}
 			},
 			fontFamily: {
 				// Add any custom fonts here
-				sans: ['JetBrains Mono Variable',...fontFamily.sans],
-				mono: ['Rubik Mono One',...fontFamily.mono]
+				sans: ["JetBrains Mono Variable", ...fontFamily.sans],
+				mono: ["Rubik Mono One", ...fontFamily.mono],
 			},
 			transitionProperty: {
 				height: "height",
